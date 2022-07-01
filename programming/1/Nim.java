e8015b5c394785ab9dd2f158a16ebf2b72b8273f
Benjamin Eckley <97626329+beckley1@users.noreply.github.com>
2022-07-01 15:16:34 +0000

    added foler 2 and ArrayPractice.java

---
+++ programming/1/Nim.java
@@ -0,0 +1,47 @@
+import java.io.*;
+import java.util.*;
+
+/* 
+  GOAL: Be the player to remove the last stone.
+  RULES:
+  - bag contains 12 stones initially
+  - a move consists of removing 1-3 stones from the bag
+  CODE:
+  - turn starts with prompting user for num of stones to remove
+  - tell user how many AI removed (random 1-3)
+  - continue until there is a winner
+*/
+
+public class Nim
+  {
+    public static void main (String[] args)
+    {
+      //Welcome message
+      System.out.println("Welcome to the Wonderful Game of Nim!");
+      //make starting bag of stones with 12 stones
+      int stones = 12;
+
+      // Round begins reading # of stones in the bag
+    System.out.printf("There are %d stones in the bag.\n", stones);
+      
+      int stonesTaken;
+
+      Scanner input = new Scanner(System.in);
+      //prompt player to pick up a stone(s)
+      public static int playerTurn(int stones){
+        
+      System.out.println ()
+
+      //loop until game ends
+      while (){
+      //prompt user input
+
+      //
+      }
+     
+
+             
+      ]
+      
+    }
+  }
