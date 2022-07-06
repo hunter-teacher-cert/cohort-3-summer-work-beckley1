70aea9d7955132522540ccf5bbbcfe0b8093c6d2
Benjamin Eckley <97626329+beckley1@users.noreply.github.com>
2022-07-06 03:39:09 +0000

    recompiled and reworked Beer.java

---
+++ pre/pre04/Beer.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.*;
+
+public class Beer{
+
+public static void bottlesOfBeer(int n){
+
+  if (n == 0){
+    System.out.println("No bottles of beer on the wall,");
+    System.out.println("no bottles of beer,");
+    System.out.println("ya’ can’t take one down, ya’ can’t pass it around,");
+    System.out.println("’cause there are no more bottles of beer on the wall!");
+    }
+    
+  else{
+    System.out.println(n + " bottles of beer on the wall,");
+    System.out.println(n + " bottles of beer,");
+    System.out.println("ya’ take one down, ya’ pass it around,");
+    System.out.println((n-1) + " bottles of beer on the wall.");
+    bottlesOfBeer(n-1);
+    }
+  }
+
+  
+public static void main(String[] args){
+  bottlesOfBeer(99);
+  
+}
+  
+}
