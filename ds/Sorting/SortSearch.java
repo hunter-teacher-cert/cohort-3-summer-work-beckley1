782693c33d2967c8e215da42f595a7685df35274
Benjamin Eckley <97626329+beckley1@users.noreply.github.com>
2022-07-19 15:28:13 +0000

    added SortSearch and its Driver

--- ds/Sorting/SortSearch.java
+++ ds/Sorting/SortSearch.java
@@ -2,24 +2,17 @@ import java.io.*;
 import java.util.*;
 
 /*
-
 Sort Project:
-
 Part 1:  (BASIC)
-  1. Analyze the two constructors - try to figure out how they work.
-  2. Compile and run the program (SortProjectDriver.java and SortProject.java) and confirm
-  the behavior of the constructors.
-
+  1. Analyze the two constructors - try to figure out how they work. DONE
+  2. Compile and run the program (SortSearchDriver.java and SortSearch.java) and confirm
+  the behavior of the constructors. DONE
   Part 2: (BASIC)
-  1. Read the description of findSmallestIndex and complete the method.
-  2. Uncomment the lines in SortProjectDriver to test.
-
+  1. Read the description of findSmallestIndex and complete the method. DONE
+  2. Uncomment the lines in SortSearchDriver to test.
   Part 3: (INTERMEDIATE)
   1. Complete the sort method - read comments for description
-  2. Uncomment the lines in sortProjectDriver to test.
-
-
-
+  2. Uncomment the lines in SortSearchDriver to test.
 Search Project:
   1. Complete the linear search (BASIC)
   2. Complete the binary search (Intermediate)
@@ -33,30 +26,34 @@ public class SortSearch{
     /* Instance Variables */
     private ArrayList<Integer> data;  // to store the data
     
-    private Random r; 
+    private Random r; //variable to hold a randomly generated object.
 
     
-    public SortSearch(){
-	data = new ArrayList<Integer>();
-	r = new Random();
-	for (int i=0;i<15;i++){
-	    data.add(r.nextInt(20));
-	}
+    public SortSearch()//constructor that has no parameters
+    {
+    	data = new ArrayList<Integer>();//initializes the instance variable "data"
+    	r = new Random();//initializes the instance variable "r".
+    	for (int i=0; i<15; i++) //default size is 15
+      {
+    	  data.add(r.nextInt(20)); //the parameter "20" fills with random values between 0 and 19
+    	}
 	
     }
     
-    public SortSearch(int size){
-	data = new ArrayList<Integer>();
-	r = new Random();
-	for (int i=0;i<size;i++){
-	    data.add(r.nextInt(20));
-	}
+    public SortSearch(int size)//overloaded constructor that takes in an int "size"
+    {
+    	data = new ArrayList<Integer>();//initializes the instance variable "data"
+    	r = new Random();//initializes the instance variable "r".
+    	for (int i=0; i<size; i++)//size is provided by the user through through the varialbe "size"
+      {
+    	  data.add(r.nextInt(20));//the parameter "20" fills with random values between 0 and 19
+    	}
 	
     }
 
     /* Convenience function to get data out of the ArrayList from the driver */
     public int get(int index){
-	return this.data.get(index);
+	    return this.data.get(index);
     }
     
 
@@ -71,31 +68,34 @@ public class SortSearch{
       if start was 2 (start at index 2, value 10) then it would return 3
       which is the index of the value 6 which is the index with the
       smallest value from start to end
-
       On the otherh and, if start was 0, then the method would
       return 1 since the value 3 is in index 1 and that is the smallest.
       
     */
-    public int findSmallestIndex(int start){
-	int smallIndex = 0;
-	
-	return smallIndex;
+    public int findSmallestIndex(int start)
+    {
+    	int smallIndex = start;
+      for(int i = start; i < data.size(); i++)
+      {
+        if(data.get(i) < data.get(smallIndex))
+        {
+          smallIndex = i;
+        }
+      }	
+    	return smallIndex;
     }
 
 
     /**
        Implement the selection sort algorithm by sorting the ArrayList
        data in place.
-
        Algorithm:
        Loop a variable that represents the ArrayList index from
        0 to the end of the ArrayList.
          For each index, find the smallest from that Location
 	 to the end of the array and swap it with that index.
-
 	 
        
-
     */
     public void sort(){
 
@@ -109,17 +109,14 @@ public class SortSearch{
     /**
        performs a linear search. Returns the index of the first occurence of
        value in the ArrayList data or -1 if not found.
-
        This works by starting at the first element and searching one element at a time 
        until either the element is found or you've looked at all the elements.
-
        This algorithm works on any ArrayList.
-
     */
     public int linearSearch(int value){
 	
 	
-	return 0; // replace this return
+	    return 0; // replace this return
     }
     
     /**
@@ -134,7 +131,7 @@ public class SortSearch{
 	//   if the item is at data.get(middle), return middle
 	//   otherwise, update high, low, and middle
 
-	return 0;
+	    return 0;
 	    
     }
     
@@ -148,18 +145,18 @@ public class SortSearch{
 
 	// refer to class discussion
 	
-	return 0;
+	    return 0;
 	    
     }
     
 	
     public String toString(){
-	return ""+data;
-    };
+	    return ""+data;
+    }
 
 
     public void builtinSort(){
-	Collections.sort(data);
+	    Collections.sort(data);
 	
     }
     
--- ds/Sorting/SortSearchDriver.java
+++ ds/Sorting/SortSearchDriver.java
@@ -13,12 +13,14 @@ public class SortSearchDriver {
 		
 	// Uncomment these to test part 2
 
-	// int i;
-	// i = ss.findSmallestIndex(0);
-	// System.out.println("ss["+ i +"] = "+ss.get(i)+" : " + ss);
+	int i;
+	i = ss.findSmallestIndex(0);
+  System.out.println("\nPrint results when started at 0:\nss[smallestIndex] = value : data");
+	System.out.println("ss["+ i +"] = "+ss.get(i)+" : " + ss);
 
-	// i = ss.findSmallestIndex(3);
-	// System.out.println("ss["+ i + "] = "+ss.get(i)+" : " + ss);
+	i = ss.findSmallestIndex(3);
+  System.out.println("\nPrint results when started at 3:\nss[smallestIndex] = value : data");
+	 System.out.println("ss["+ i + "] = "+ss.get(i)+" : " + ss);
 	
 	// Uncomment these to test part 3
 
